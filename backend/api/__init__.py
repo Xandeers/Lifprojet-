551db5c4eb586aa444078d889a6633db9667fd3c
OtmaneElm10 <elmoukiotmane10@gmail.com>
2025-02-18 14:37:46 +0100

    Revert "supprime doublons"
    This reverts commit 9ece0fd1958901468111ba771eb96081f6a6cbbc.

---
+++ backend/api/__init__.py
@@ -0,0 +1,40 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager
+from flask_marshmallow import Marshmallow
+from flask_cors import CORS
+from dotenv import load_dotenv
+from os import getenv
+from werkzeug.exceptions import HTTPException
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Flask Extensions
+db = SQLAlchemy()
+jwt = JWTManager()
+ma = Marshmallow()
+cors = CORS(supports_credentials=True)
+
+def create_app():
+    # Flask App Instance
+    app = Flask(__name__)
+
+    # Flask App Settings
+    app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['JWT_SECRET_KEY'] = getenv('JWT_SECRET_KEY')
+    app.config['JWT_TOKEN_LOCATION'] = ["cookies"]
+
+    # Initialization of Flask Extensions
+    db.init_app(app)
+    jwt.init_app(app)
+    ma.init_app(app)
+    cors.init_app(app)
+
+    # Register Blueprint
+    from .routes import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # Return Flask app instance
+    return app
---
+++ backend/api/routes.py
@@ -0,0 +1,85 @@
+from flask import Blueprint, jsonify
+from webargs import fields
+from webargs.flaskparser import use_args, parser
+from flask_jwt_extended import create_access_token, set_access_cookies, unset_jwt_cookies, jwt_required, get_jwt_identity
+
+from . import db
+from .models import User, user_schema
+
+main = Blueprint('main', __name__)
+
+# Error parsing for bad request
+@parser.error_handler
+def handle_request_parsing_error(err, req, schema, *, error_status_code, error_headers):
+    response = {
+        'error': 'Validation Error',
+        'msg': err.messages
+    }
+    return jsonify(response), 400
+
+# Auth Routes
+@main.route('/register', methods=['POST'])
+@use_args({
+    'username': fields.Str(required=True),
+    'email': fields.Str(required=True),
+    'password': fields.Str(required=True),
+}, location='json')
+def register(args):
+    # Check if user already exists
+    username = args['username']
+    email = args['email']
+
+    if User.is_username_taken(username):
+        return jsonify({"error": "Username already exists"}), 400
+    if User.is_email_taken(email):
+        return jsonify({"error": "Email already exists"}), 400
+        
+    # Create a new user
+    new_user = User(username, email, args['password'])
+
+    # Add to database
+    db.session.add(new_user)
+    db.session.commit()
+
+    return user_schema.jsonify(new_user), 201
+
+@main.route('/login', methods=['POST'])
+@use_args({
+    'email': fields.Str(required=True),
+    'password': fields.Str(required=True)
+}, location='json')
+def login(args):
+    email = args['email']
+    password = args['password']
+
+    # Search user
+    user = User.query.filter_by(email=email).first()
+    if user and user.check_password(password):
+        response = jsonify({"msg": 'Login successful'})
+        access_token = create_access_token(identity=str(user_schema.jsonify(user)))
+        set_access_cookies(response, access_token)
+        return response
+
+    else:
+        return jsonify({
+            'error': 'Invalid credentials'
+        }), 401
+    
+@main.route('/logout', methods=["POST"])
+def logout():
+    response = jsonify({"msg": "Logout successful"})
+    unset_jwt_cookies(response)
+    return response
+    
+@main.route('/profile', methods=['GET'])
+@jwt_required()
+def profile():
+    user_id = get_jwt_identity()
+    user = User.query.get(user_id)
+
+    if not user:
+        return jsonify({
+            'msg': 'User not found'
+        }), 404
+    
+    return user_schema.jsonify(user), 200
