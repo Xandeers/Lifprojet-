4d0aed3ac5640ddc7ba2dc6a5e797ce5b8d9630d
Smail <141525170+yasm3@users.noreply.github.com>
2025-04-21 14:27:19 +0200

    fts product

--- backendv2/app/models/product.py
+++ backendv2/app/models/product.py
@@ -1,6 +1,7 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.dialects.postgresql import TSVECTOR
+from sqlalchemy.orm import Mapped, mapped_column, Session
 from enum import Enum as PyEnum
-from sqlalchemy import Enum
+from sqlalchemy import Enum, Column, text
 from app.database import Base
 
 class ProductCategory(PyEnum):
@@ -26,5 +27,19 @@ class Product(Base):
     fruits_veg: Mapped[float]
     fibers: Mapped[float]
 
+    search_field = Column(TSVECTOR) # FTS
+
     source: Mapped[str]  # "ciqual", "off"...
     barcode: Mapped[str] = mapped_column(nullable=True)  # pour OFF seulement
+
+def search_products_fts(db: Session, query: str):
+    sql = text("""
+                   SELECT 
+                       *,
+                       ts_rank(search_field, websearch_to_tsquery('french', :query)) AS rank
+                   FROM products
+                   WHERE search_field @@ websearch_to_tsquery('french', :query)
+                   ORDER BY rank DESC
+                   LIMIT 15
+               """)
+    return db.execute(sql, {"query": query}).mappings().all()
--- backendv2/app/routes/product.py
+++ backendv2/app/routes/product.py
@@ -5,7 +5,7 @@ from fastapi.params import Depends
 from sqlalchemy.orm import Session
 
 from app.database import get_db
-from app.models import Product
+from app.models import search_products_fts
 from app.schemas.product import ProductBase
 
 router = APIRouter()
@@ -13,4 +13,6 @@ router = APIRouter()
 
 @router.get("/search")
 async def search_by_text(query: str, db: Session = Depends(get_db)) -> List[ProductBase]:
-    return db.query(Product).all() #type: ignore
+    results = search_products_fts(db, query)
+    products = [ProductBase(**row) for row in results]
+    return products
